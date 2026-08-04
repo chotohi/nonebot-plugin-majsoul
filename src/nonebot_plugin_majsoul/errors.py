5272b9a5c622afecd12ddb25528070df5461fdec
chotohi <151453533+chotohi@users.noreply.github.com>
2026-08-04 12:10:27 +0800

    添加PaifuyaRateLimitError相关logger.warning，便于调试
    Add custom error handling for PaifuyaRateLimitError.

--- src/nonebot_plugin_majsoul/errors.py
+++ src/nonebot_plugin_majsoul/errors.py
@@ -4,7 +4,19 @@ from httpx import HTTPError
 from nonebot import logger
 from ssttkkl_nonebot_utils.errors.error_handler import ErrorHandlers
 
+
+class PaifuyaRateLimitError(Exception):
+    def __init__(self):
+        super().__init__("paifuya connection temporarily unavailable")
+
+
 error_handlers = ErrorHandlers()
+
+
+@error_handlers.register(PaifuyaRateLimitError)
+def _(e):
+    logger.warning(str(e))
+    return "连接超时，请稍后再试"
 
 
 @error_handlers.register(HTTPError)
